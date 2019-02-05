79065f5c0207c1983528653f4f7facc50b5f1505
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2019-02-05 14:03:15 +0700

    Support image upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,36 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import ReactFilestack from 'filestack-react'
 
-export default () => (
-  <Fragment>
-    <h1>Welcome!</h1>
-    <Link to="/signup">Sign Up</Link>
-    <Link to="/signin">Sign In</Link>
-  </Fragment>
-)
+class HomePage extends Component {
+  state = {}
+
+  handleSuccess = result => {
+    const imageUrl = result.filesUploaded[0].url
+
+    console.log(imageUrl)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Welcome!</h1>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/signin">Sign In</Link>
+        <ReactFilestack
+          apikey={process.env.REACT_APP_FILESTACK_API_KEY}
+          buttonText="Click me"
+          options={{
+            accept: 'image/*',
+            storeTo: {
+              location: 's3'
+            }
+          }}
+          onSuccess={this.handleSuccess}
+        />
+      </Fragment>
+    )
+  }
+}
+
+export default HomePage
